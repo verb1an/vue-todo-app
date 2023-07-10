cd19b1a73ed9421e2fa98039c2d375379706fac4
verb1an <tabyshkinalex@gmail.com>
2023-07-10 12:36:03 +0700

    v9.2.0

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-        ? '/vue-todo-app/'
-        : '/'
-})
+    transpileDependencies: true,
+    publicPath: process.env.NODE_ENV === "production" ? "/vue-todo-app/dist/" : "/",
+});
